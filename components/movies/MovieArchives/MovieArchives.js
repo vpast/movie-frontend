69de8ca37006f575bd26d5382b1aa900f137202d
vpast <dalverq@gmail.com>
2021-10-27 15:37:00 +0300

    archives pagination completed

--- components/movies/MovieArchives/MovieArchives.js
+++ components/movies/MovieArchives/MovieArchives.js
@@ -2,35 +2,55 @@ import { useState, useEffect } from "react";
 import MoviePagination from "../MoviePagination";
 import MovieCard from "../MovieCard";
 import MovieLayout from "../../layouts/Movie";
-import { API_URL } from "../../../config"
+import { API_URL } from "../../../config";
 import MovieArchivesFilter from "../MovieArchives/MovieArchivesFilter";
 
+const PAGE_LIMIT = 10;
 const MovieArchives = () => {
   const [movies, setMovies] = useState([]);
+  const [activePage, setActivePage] = useState(1);
+  const [pages, setPages] = useState(1);
   useEffect(() => {
-    fetch(`${API_URL}/movies?archive=true`)
+    const queryParams = [
+      `skip=${(activePage - 1) * PAGE_LIMIT}`,
+      `limit=${PAGE_LIMIT}`,
+    ];
+
+    fetch(`${API_URL}/movies?archive=true&${queryParams.join("&")}`)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.items);
+        setPages(Math.ceil(data.count / PAGE_LIMIT));
       });
-  }, []);
+  }, [activePage]);
   return (
-    <MovieLayout title="Archives" items={[{
-      title: "Home",
-      url: "/"
-    },{
-      title: "Movies",
-      url: "/movies"
-    },{
-      title: "Archives"
-    }]}>
+    <MovieLayout
+      title="Archives"
+      items={[
+        {
+          title: "Home",
+          url: "/",
+        },
+        {
+          title: "Movies",
+          url: "/movies",
+        },
+        {
+          title: "Archives",
+        },
+      ]}
+    >
       <section className="section-long">
         <div className="container">
           <MovieArchivesFilter />
           {movies.map((item) => {
             return <MovieCard item={item} key={item._id} />;
           })}
-          <MoviePagination />
+          <MoviePagination
+            activePage={activePage}
+            setActivePage={setActivePage}
+            pages={pages}
+          />
         </div>
       </section>
     </MovieLayout>

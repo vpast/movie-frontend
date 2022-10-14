5eca5f17e961a8401d96c428311a3f4e337f3abb
vpast <vpast.dev@gmail.com>
2022-10-14 16:05:57 +0300

    logo fix

--- components/movies/MovieHeader.js
+++ components/movies/MovieHeader.js
@@ -3,23 +3,29 @@ import { useState } from 'react';
 
 const MovieHeader = () => {
   const [menuActive, setMenuActive] = useState(false);
-  
+
   const phoneMenuHandler = () => {
     setMenuActive(!menuActive);
   };
-  
+
   return (
-    <header id='header' className={menuActive ? 'header header-horizontal header-view-pannel active' : 'header header-horizontal header-view-pannel'}>
+    <header
+      id='header'
+      className={
+        menuActive
+          ? 'header header-horizontal header-view-pannel active'
+          : 'header header-horizontal header-view-pannel'
+      }
+    >
       <div className='container'>
         <nav className='navbar'>
           <Link href='/'>
             <a className='navbar-brand'>
               <span className='logo-element'>
                 <span className='logo-tape'>
-                  <span
-                    className='svg-content svg-fill-theme'
-                    data-svg='/memico/images/svg/logo-part.svg'
-                  />
+                  <span className='svg-content svg-fill-theme'>
+                    <svg src='https://svgshare.com/i/nPD.svg' xmlns="http://www.w3.org/2000/svg" style={{height:'1.2em', width:'1.5349em'}}><path class="fill-path" fill-rule="evenodd" clip-rule="evenodd" d="M8 0H47V3H51V0H55V43H51V40H47V43H8V40H4V43H0V0H4V3H8V0ZM8 7H4V11H8V7ZM4 15H8V19H4V15ZM8 23H4V27H8V23ZM4 32H8V36H4V32ZM51 7H47V11H51V7ZM47 15H51V19H47V15ZM51 23H47V27H51V23ZM47 32H51V36H47V32Z" fill="#FF8A00"></path></svg>
+                  </span>
                 </span>
                 <span className='logo-text text-uppercase'>
                   <span>M</span>emico

da4b552229b407ec7c5dfe7a71d0cd688eee0f83
vpast <vpast.dev@gmail.com>
2022-10-14 17:07:55 +0300

    gray block fix and logo error fixes

--- components/movies/MovieFooter.js
+++ components/movies/MovieFooter.js
@@ -18,9 +18,9 @@ const MovieFooter = () => {
                       xmlns='http://www.w3.org/2000/svg'
                     >
                       <path
-                        class='fill-path'
-                        fill-rule='evenodd'
-                        clip-rule='evenodd'
+                        className='fill-path'
+                        fillRule='evenodd'
+                        clipRule='evenodd'
                         d='M8 0H47V3H51V0H55V43H51V40H47V43H8V40H4V43H0V0H4V3H8V0ZM8 7H4V11H8V7ZM4 15H8V19H4V15ZM8 23H4V27H8V23ZM4 32H8V36H4V32ZM51 7H47V11H51V7ZM47 15H51V19H47V15ZM51 23H47V27H51V23ZM47 32H51V36H47V32Z'
                         fill='#FF8A00'
                       ></path>
